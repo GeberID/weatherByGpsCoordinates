a72dcc5f579227410616e44fbcfc0b5a39a81ad0
Alexander <aleksanderfranorge@gmail.com>
2026-01-02 17:03:36 +0300

    первый рабочий вариант

--- weather/weather.py
+++ weather/weather.py
@@ -1,9 +1,21 @@
 from coordinates import get_gps_coordinates
+from exceptions import CantGetCoordinates
+from weather.exceptions import ApiServiceError
 from weather_api import get_weather
 from weather_printer import format_weather
 
-coordinates = get_gps_coordinates()
-weather = get_weather(coordinates)
+def main():
+    try:
+        coordinates = get_gps_coordinates()
+    except CantGetCoordinates:
+        print("Не смог получить GPS-координаты")
+        exit(1)
+    try:
+        weather = get_weather(coordinates)
+    except ApiServiceError:
+        print("Не смог получить погоду в API-сервиса погоды")
+        exit(1)
+    print(format_weather(weather))
 
 if __name__ == '__main__':
-    print(format_weather(weather))
+    main()
